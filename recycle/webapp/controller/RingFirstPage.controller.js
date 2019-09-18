d2c5e7d96ccc8177eca8911383f2f7f254af782e
eduardtsatourov <eduard.tsatourov@ordina.be>
2019-09-18 11:32:15 +0000

    initial push

--- recycle/webapp/controller/RingFirstPage.controller.js
+++ recycle/webapp/controller/RingFirstPage.controller.js
@@ -4,16 +4,17 @@ sap.ui.define([
 ], function (Controller, UIComponent) {
 	"use strict";
 
-	return Controller.extend("opensap.recycle.controller.Home", {
-		
+	return Controller.extend("opensap.recycle.controller.RingFirstPage", {
+
 		onInit: function () {
-		
+
 		},
 
 		onPress: function (oEvent) {
 			var oRouter = UIComponent.getRouterFor(this);
-			oRouter.navTo("RouteRingFirstPage");
+			oRouter.navTo("RouteRingSecondPage");
 		}
+
 	});
 
 });
