0e8704a4645cc18072d4771ea2addad8cd7ed91a
Matthijs Huyghe <matthijs.huyghe@ordina.be>
2019-09-25 09:23:10 +0000

    Add map & location

--- recycle/webapp/controller/ItemBack.controller.js
+++ recycle/webapp/controller/ItemBack.controller.js
@@ -2,7 +2,9 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
-
+	
+	var map, longitude, latitude, markerVectorLayer;
+	
 	return Controller.extend("opensap.recycle.controller.ItemBack", {
 
 		/**
@@ -12,7 +14,7 @@ sap.ui.define([
 		 */
 		onInit: function () {
 
-		}
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -28,9 +30,89 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf opensap.recycle.view.ItemBack
 		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+		onAfterRendering: function() {
+			var that = this;
+			
+			if (!this.initialized) {
+				this.initialized = true;
+				
+				// Set OpenLayers map
+				map = new ol.Map({
+					target: that.getView().byId("map_canvas").getDomRef(),
+					layers: [
+						new ol.layer.Tile({
+							source: new ol.source.OSM()
+						})
+					],
+					view: new ol.View({
+						center: ol.proj.fromLonLat([74.006, 40.7127]),
+						zoom: 10
+					})
+				});
+			
+				if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition(function (position) {
+						longitude = position.coords.longitude;
+						latitude = position.coords.latitude;
+						
+						// Zoom the map
+						map.getView().setCenter(ol.proj.transform([longitude, latitude], 'EPSG:4326', 'EPSG:3857'));
+						map.getView().setZoom(15);
+						
+						// If there is already a maker (= layer), delete it as only one marker is allowed
+						map.removeLayer(markerVectorLayer);
+						
+						// Create a marker (= feature) which references to the location of the user
+						var marker = new ol.Feature({
+								geometry: new ol.geom.Point(
+								ol.proj.fromLonLat([longitude, latitude])
+							),  // Cordinates of New York's Town Hall
+						});
+						
+						// Change the style of the marker and use a custom icon
+						marker.setStyle(new ol.style.Style({
+								image: new ol.style.Icon({
+									src: 'icon/icon.png'
+							})
+						}));
+						
+						// Create a vector based on the marker (= feature)
+						var vectorSource = new ol.source.Vector({
+							features: [marker]
+						});
+							
+						// Create layer based on a vector	
+						markerVectorLayer = new ol.layer.Vector({
+							source: vectorSource,
+						});
+						
+						// Add layer to the map
+						map.addLayer(markerVectorLayer);
+						
+						// Update text value of longitude & latitude
+						if(latitude && longitude) {
+							var coor = [longitude,latitude];
+							
+							// Convert coordinates to DMS
+							var dms = ol.coordinate.toStringHDMS(coor);
+							
+							// Get north & east
+							var index = dms.indexOf("N");
+							var north = dms.substr(0, index + 1); // Gets the first part
+							var east = dms.substr(index -1);  // Gets the text part
+
+							that.getView().byId("north").setText(north);
+							that.getView().byId("east").setText(east);
+						} else {
+							that.getView().byId("north").setText("North hasn't been set");
+							that.getView().byId("east").setText("East hasn't been set");
+						}
+					});
+				} else {
+					sap.m.MessageToast.show("Fail");
+				}
+			}
+		}
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
--- recycle/webapp/controller/SendLocation.controller.js
+++ recycle/webapp/controller/SendLocation.controller.js
@@ -3,8 +3,10 @@ sap.ui.define([
 	"sap/ui/core/UIComponent"
 ], function (Controller, UIComponent) {
 	"use strict";
+	
+	var map;
+	
 	return Controller.extend("opensap.recycle.controller.SendLocation", {
-
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
@@ -15,9 +17,16 @@ sap.ui.define([
 		},
 		
 		onPressSend : function (oEvent) {
-			var oRouter = UIComponent.getRouterFor(this);
-			oRouter.navTo("ItemBack");
-		}
+			// Ask the user to share its location
+			if (navigator.geolocation) {
+					// Go to the next screen if user shares its location
+					var oRouter = UIComponent.getRouterFor(this);
+					oRouter.navTo("ItemBack");
+			} else {
+				// Throw a warning messagetoast when user doesn't share its location
+				sap.m.MessageToast.show("Gelieve uw locatie te delen");
+			}
+		},
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -33,9 +42,27 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf opensap.recycle.view.SendLocation
 		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+		onAfterRendering: function() {
+			var that = this;
+			
+			if (!this.initialized) {
+				this.initialized = true;
+				
+				// Set OpenLayers map
+				map = new ol.Map({
+					target: that.getView().byId("map_canvas").getDomRef(),
+					layers: [
+						new ol.layer.Tile({
+							source: new ol.source.OSM()
+						})
+					],
+					view: new ol.View({
+						center: ol.proj.fromLonLat([4.357582, 51.198185]),
+						zoom: 12
+					})
+				});
+			}
+		}
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
